e60b66abf830203b73e23a09e4a507958902228c
Nathan Springman <46718540+nspringman@users.noreply.github.com>
2019-07-22 10:11:22 -0500

    Update Setup-API-Admin.py
    -Run FGT commands without modyfing netmiko files. Expected response is "#".
    --Updates to nemiko might revert changes.
    --Would likely make netmiko incompatible with other devices if used again.
    
    -Outputs API key to text file so can be run with double-click rather than through IDLE

--- Setup-API-Admin.py
+++ Setup-API-Admin.py
@@ -7,7 +7,7 @@ import sys
 
 
 
-#Netmiko will not accept a variable. This needs to be resolved. 
+#Netmiko will not accept a variable. This needs to be resolved.
 net_connect = Netmiko(
     "192.168.1.99",
     username="admin",
@@ -17,49 +17,53 @@ net_connect = Netmiko(
 
 
 
-print(net_connect.find_prompt())
+# print(net_connect.find_prompt())
+#
+# net_connect.set_base_prompt('#')
+# net_connect.auto_find_prompt = False
+def fgt_command(command):
+    return net_connect.send_command(command, expect_string='#')
 
-output = net_connect.send_command('config system accprofile')
-output = net_connect.send_command('edit "readWrite"')
-output = net_connect.send_command('set mntgrp read-write')
-output = net_connect.send_command('set admingrp read-write')
-output = net_connect.send_command('set updategrp read-write')
-output = net_connect.send_command('set authgrp read-write')
-output = net_connect.send_command('set sysgrp read-write')
-output = net_connect.send_command('set netgrp read-write')
-output = net_connect.send_command('set loggrp read-write')
-output = net_connect.send_command('set routegrp read-write')
-output = net_connect.send_command('set fwgrp read-write')
-output = net_connect.send_command('set vpngrp read-write')
-output = net_connect.send_command('set utmgrp read-write')
-output = net_connect.send_command('set endpoint-control-grp read-write')
-output = net_connect.send_command('set wifi read-write')
-output = net_connect.send_command('end')
+output = fgt_command('config system accprofile')
+output = fgt_command('edit "readWrite"')
+output = fgt_command('set mntgrp read-write')
+output = fgt_command('set admingrp read-write')
+output = fgt_command('set updategrp read-write')
+output = fgt_command('set authgrp read-write')
+output = fgt_command('set sysgrp read-write')
+output = fgt_command('set netgrp read-write')
+output = fgt_command('set loggrp read-write')
+output = fgt_command('set routegrp read-write')
+output = fgt_command('set fwgrp read-write')
+output = fgt_command('set vpngrp read-write')
+output = fgt_command('set utmgrp read-write')
+output = fgt_command('set endpoint-control-grp read-write')
+output = fgt_command('set wifi read-write')
+output = fgt_command('end')
 
-output = net_connect.send_command('config system api-user')
-output = net_connect.send_command('edit api-admin')
-output = net_connect.send_command('set accprofile "readWrite"')
-output = net_connect.send_command('set api-key RaSzoz68vMGesyaIYTtJrhbpbvf4da')
-output = net_connect.send_command('api-key')
-output = net_connect.send_command('config trusthost')
-output = net_connect.send_command('edit 1')
-output = net_connect.send_command('set ipv4-trusthost 192.168.0.0 255.255.0.0')
-output = net_connect.send_command('next')
-output = net_connect.send_command('config trusthost')
-output = net_connect.send_command('edit 2')
-output = net_connect.send_command('set ipv4-trusthost 10.0.0.0 255.00.0.0')
-output = net_connect.send_command('next')
-output = net_connect.send_command('edit 3')
-output = net_connect.send_command('set ipv4-trusthost 66.0.0.2 255.255.255.255')
-output = net_connect.send_command('next')
-output = net_connect.send_command('end')
-output = net_connect.send_command('end')
-output = net_connect.send_command('end')
+output = fgt_command('config system api-user')
+output = fgt_command('edit api-admin')
+output = fgt_command('set accprofile "readWrite"')
+output = fgt_command('set api-key RaSzoz68vMGesyaIYTtJrhbpbvf4da')
+output = fgt_command('api-key')
+output = fgt_command('config trusthost')
+output = fgt_command('edit 1')
+output = fgt_command('set ipv4-trusthost 192.168.0.0 255.255.0.0')
+output = fgt_command('next')
+output = fgt_command('config trusthost')
+output = fgt_command('edit 2')
+output = fgt_command('set ipv4-trusthost 10.0.0.0 255.0.0.0')
+output = fgt_command('next')
+output = fgt_command('edit 3')
+output = fgt_command('set ipv4-trusthost 66.0.0.2 255.255.255.255')
+output = fgt_command('next')
+output = fgt_command('end')
+output = fgt_command('end')
+output = fgt_command('end')
 
+output = fgt_command('execute api-user generate-key api-admin')
 
-output = net_connect.send_command('execute api-user generate-key api-admin')
-
-print(output)
+with open('API_KEY.txt', 'w+') as API_KEY:
+    API_KEY.write(output)
 
 net_connect.disconnect()
-
